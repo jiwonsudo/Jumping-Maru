3ef906e990c5f146b7ac6cc43ae28cc85c226eb3
jiwonsudo <wondev112@gmail.com>
2024-01-08 03:10:11 +0900

    Feat: use only space for jump, Refactor: outer file removed

--- jump.js
+++ jump.js
@@ -2,6 +2,8 @@ const maru = document.querySelector('.maru');
 
 const removeJump = () => maru.classList.remove('maru-jump');
 document.addEventListener('keydown', (event) => {
-  maru.classList.add('maru-jump');
-  setTimeout(removeJump, 500);
+  if (event.code === 'Space') {
+    maru.classList.add('maru-jump');
+    setTimeout(removeJump, 501);
+  }
 });
